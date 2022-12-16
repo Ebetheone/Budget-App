37a5f070f2b425d151240ad7130ba4b10549da2e
Elbeg <elbeg.db@yahoo.com>
2022-12-17 02:37:40 +0800

    feat: fix

--- src/widgets/SettingsTable/Modal/EditUser.js
+++ src/widgets/SettingsTable/Modal/EditUser.js
@@ -1,8 +1,10 @@
+import React from "react";
 import { Modal, Button, Form, Input } from "antd";
 import "antd/dist/antd.css";
-import React from "react";
+
 import { useEditUser } from "./useEditUser";
 import { ScaleLoader } from "react-spinners";
+
 const layout = {
   labelCol: {
     span: 8,
@@ -11,6 +13,7 @@ const layout = {
     span: 16,
   },
 };
+
 const EditUser = ({ setHandleEdit, handleEdit, loading }) => {
   const { editUser } = useEditUser();
   return (
@@ -44,7 +47,11 @@ const EditUser = ({ setHandleEdit, handleEdit, loading }) => {
                 <Input />
               </Form.Item>
               <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-                <Button type="primary" htmlType="submit">
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  onClick={() => setHandleEdit(false)}
+                >
                   Submit
                 </Button>
               </Form.Item>
--- src/widgets/UserTable/Modal/AddUser.js
+++ src/widgets/UserTable/Modal/AddUser.js
@@ -14,6 +14,7 @@ const layout = {
     span: 16,
   },
 };
+
 const AddUser = ({ setVisible, visible, loading, id }) => {
   const { addUser } = useEditUser(id);
   return (
