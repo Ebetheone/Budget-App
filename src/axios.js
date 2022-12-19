bc5c7f2f820406a78db63056ffb60586ef73cb07
Elbeg <elbeg.db@yahoo.com>
2022-12-19 12:27:21 +0800

    feat: fix

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-// "https://budget-app-backend-5n3r418f1-ebetheone.vercel.app/";
-const BASE_URL = "http://localhost:4000/";
+const BASE_URL = "https://budget-app-backend-5n3r418f1-ebetheone.vercel.app/";
 // axios.defaults.withCredentials = true;
 export default axios.create({
   baseURL: BASE_URL,
