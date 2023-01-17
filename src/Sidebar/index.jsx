f36bff60b1642ab4479dbec94d1cb31ad386611d
Elbeg <elbeg.db@yahoo.com>
2023-01-18 01:26:34 +0800

    feat: fix

--- src/Sidebar/index.jsx
+++ src/Sidebar/index.jsx
@@ -39,7 +39,7 @@ const Sidebar = () => {
             alignItems: "center",
           }}
         >
-          <div className="logo">Elbeg LLC</div>
+          <div className="logo">Ebetheone</div>
           {categories.map((dat) => (
             <Menu.Item eventKey={dat.key} key={dat.key} icon={dat.icon}>
               <Link to={dat.path}>{dat.label}</Link>
